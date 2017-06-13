e91fe869f5608ff35aed8c2c35430e09f19c6bfb
Austin Knight <austin.knight@insidesales.com>
2017-06-13 13:34:18 -0600

    Add code syntax highlighting

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Docs from './docs/Docs';
 import './index.css';
+import '../node_modules/highlight.js/styles/ocean.css';
 
 ReactDOM.render(<Docs />, document.getElementById('root'));
