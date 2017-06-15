ef56e1370e959a1ceec632c77c898f3c4e8f6d56
Austin Knight <austin.knight@insidesales.com>
2017-06-15 11:46:21 -0600

    Adding some more test components

---
+++ src/docs/examples/Label/ExampleRequried.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Label from 'ui-components/Label';
+
+/** Required label */
+export default function ExampleRequired() {
+  return <Label htmlFor="test" label="test" required />
+}
---
+++ src/docs/examples/ProgressBar/Example10Percent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ProgressBar from 'ui-components/ProgressBar';
+
+/** 10% progress */
+export default function Example10Percent() {
+  return <ProgressBar percent={10} width={150} />
+}
