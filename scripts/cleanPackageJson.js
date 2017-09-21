60a90ce6de8292b73aa75e0d07fc705dea8b2fb5
Austin Knight <austinknight@users.noreply.github.com>
2017-09-21 16:09:56 -0600

    feat(build): clean package json on build for release. test break versioning.
    BREAKING CHANGE: breaks nothing but testing

---
+++ scripts/cleanPackageJson.js
@@ -0,0 +1,60 @@
+var path = require('path');
+var fse = require('fs-extra');
+
+function createPackageFile() {
+  return new Promise((resolve) => {
+    fse.readFile(path.resolve(__dirname, '../package.json'), 'utf8', (err, data) => {
+      if (err) {
+        throw err;
+      }
+
+      resolve(data);
+    });
+  })
+  .then((data) => JSON.parse(data))
+  .then((packageData) => {
+    const {
+      author,
+      version,
+      description,
+      keywords,
+      repository,
+      license,
+      bugs,
+      homepage,
+      peerDependencies,
+      dependencies,
+      scripts
+    } = packageData;
+
+    const minimalPackage = {
+      name: 'isdc-components-test',
+      author,
+      version,
+      description,
+      main: './index.js',
+      keywords,
+      repository,
+      license,
+      bugs,
+      homepage,
+      peerDependencies,
+      dependencies,
+      scripts: {
+        'semantic-release': scripts['semantic-release']
+      }
+    };
+
+    return new Promise((resolve) => {
+      const libPath = path.resolve(__dirname, '../lib/package.json');
+      const data = JSON.stringify(minimalPackage, null, 2);
+      fse.writeFile(libPath, data, (err) => {
+        if (err) throw (err);
+        console.log(`Created package.json in ${libPath}`);
+        resolve();
+      });
+    });
+  });
+}
+
+createPackageFile();
