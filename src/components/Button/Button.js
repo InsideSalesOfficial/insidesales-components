3c316e5d57ecc575c94fda2cdc8b3019d6cf5324
Austin Knight <austinknight@users.noreply.github.com>
2017-09-22 09:28:15 -0600

    fix(build): Uncomment auto-release in travis and make a trivial change in Button to test versioning.

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
-const Button = props => <button type="button">{props.children}</button>;
+const Button = props => 
+  <button type="button">
+    {props.children}
+  </button>;
 
 export default Button;
