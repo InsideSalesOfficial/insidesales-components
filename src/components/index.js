bc3f17700941513a7b5edb380a22c4757098e8f8
Sean Duncan <seaneduncan@gmail.com>
2017-12-11 10:50:17 -0700

    fix(StepChain): forgot to export step chain… (#31)

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,7 @@ export {default as Button} from './Button';
 export {default as EditableSelectInput} from './EditableSelectInput';
 export {default as RangeSlider} from './RangeSlider';
 export {default as SelectInput} from './SelectInput';
+export {default as StepChain} from './StepChain';
 export {default as TextareaInput} from './TextareaInput';
 export {default as TextInput} from './TextInput';
 export {default as ToggleSlider} from './ToggleSlider';
