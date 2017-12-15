1ae27f430150823432d787b132b4676eab057732
Austin Knight <austinknight@users.noreply.github.com>
2017-12-15 13:53:17 -0700

    fix(SortableList): Add missing SortableList export

--- src/components/index.js
+++ src/components/index.js
@@ -5,6 +5,7 @@ export {default as InteractiveElement} from './InteractiveElement';
 export {default as OverflowMenu} from './OverflowMenu';
 export {default as RangeSlider} from './RangeSlider';
 export {default as SelectInput} from './SelectInput';
+export {default as SortableList} from './SortableList';
 export {default as StepChain} from './StepChain';
 export {default as TextareaInput} from './TextareaInput';
 export {default as TextInput} from './TextInput';
