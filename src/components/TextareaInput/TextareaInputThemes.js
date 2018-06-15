d399b7809744f2d35a05b7fd7ade729d77ada04d
Kathryn Rasmussen <kathryn.rasmussen@ITs-MacBook-Pro-8.local>
2018-06-15 14:44:42 -0600

    added dark theme to TextareaInput

---
+++ src/components/TextareaInput/TextareaInputThemes.js
@@ -0,0 +1,9 @@
+import { darkBlue, white } from "../styles/colors";
+
+export const darkTheme = {
+  background: darkBlue.darkBlue,
+  valueColor: white.white90,
+  labelColor: white.white60,
+  borderColor: white.white40,
+  focusedColor: white.white60,
+}
--- src/components/TextareaInput/index.js
+++ src/components/TextareaInput/index.js
@@ -1,1 +1,2 @@
 export { default } from './TextareaInput';
+export { darkTheme } from './TextareaInputThemes';
