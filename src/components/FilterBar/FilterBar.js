fe5a5e0e77fcd36cf99d36166d81198617f0a61f
Brian Cozzens <cozzbp@users.noreply.github.com>
2018-06-19 15:36:32 -0600

    fix(FilterBar): Export FilterBar (#124)
    * feat(Filterbar): Add filterbar component
    
    * clean up
    
    * Export FilterBar
    
    * Fix searchbar padding
    
    * Reset search on close

--- src/components/FilterBar/FilterBar.js
+++ src/components/FilterBar/FilterBar.js
@@ -11,7 +11,7 @@ import { InputItem } from '../TextInput/TextInput';
 import { colors, boxShadows } from '../styles';
 
 const FilterBarWrapper = styled.div`
-  background-color: ${colors.darkBlue};
+  background-color: ${colors.darkBlueD};
   display: flex;
   flex-direction: row;
   align-items: center;
@@ -21,6 +21,7 @@ const FilterBarWrapper = styled.div`
 
 const SearchBarWrapper = styled(FilterBarWrapper)`
   background-color: ${colors.white};
+  padding: 0 21px 0 19px;
 `;
 
 const StyledSelectInput = styled(SelectInput)`
@@ -42,13 +43,9 @@ const StyledInteractiveElement = styled(InteractiveElement)`
   display: flex;
 `;
 
-const SearchIcon = styled(Icons.SearchMaterialIcon)`
-  padding: 0 19px;
-`
-
-const CloseIcon = styled(Icons.CloseIcon)`
-  padding: 0 21px;
-`
+const StyledInputItem = styled(InputItem)`
+  padding-left: 19px;
+`;
 
 class FilterBar extends React.Component {
 
@@ -63,28 +60,36 @@ class FilterBar extends React.Component {
   showSearch = () => {
     this.setState({
       showSearch: true
-    })
+    });
   }
 
   hideSearch = () => {
+    this.props.onSearchChange('');
     this.setState({
       showSearch: false
-    })
+    });
+  }
+
+  onSearchChange = (e) => {
+    if (e) {
+      e.preventDefault();
+    }
+    this.props.onSearchChange(_.get(e, 'target.value'));
   }
 
   render() {
     if(this.state.showSearch) {
-      return (
+    return (
         <SearchBarWrapper>
-          <SearchIcon fill={colors.black60}/>
-          <InputItem
+          <Icons.SearchMaterialIcon fill={colors.black60}/>
+          <StyledInputItem
             type={'text'}
-            onChange={this.props.onSearchChange}
+            onChange={this.onSearchChange}
             placeholder={this.props.searchPlaceholder}
             autoFocus/>
           <StyledInteractiveElement onClick={this.hideSearch}>
-              <CloseIcon fill={colors.black60}/>
-            </StyledInteractiveElement>
+            <Icons.CloseIcon fill={colors.black60}/>
+          </StyledInteractiveElement>
         </SearchBarWrapper>
       );
     }
